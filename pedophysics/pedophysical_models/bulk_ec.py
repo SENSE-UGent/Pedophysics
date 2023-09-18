7b1d8ee0438c75fa5f760eb5b958b57a03b5aa03
gmendoza <gaston.mendozaveirana@ugent.be>
2023-09-18 15:01:42 +0200

    18sept_aftrnoon2

--- pedophysics/pedophysical_models/bulk_ec.py
+++ pedophysics/pedophysical_models/bulk_ec.py
@@ -3,50 +3,30 @@ from scipy.constants import pi, epsilon_0
 
 def WunderlichEC(water, ec_init, wat_init, wc, Lw):  
     """
-    Calculate the soil bulk real electrical conductivity using the Wunderlich model.
+        Wunderlich et.al 2013 
+        
+        Parameters
+        ----------
+        water: float
+            volumetric water content [-]
+        
+        ec_init: float
+            minimum electrical conductivity [S/m]
+            
+        wat_init: float
+            minimum volumetric water content [-]
+            
+        wc: float
+            Soil water real electrical conductivity [-]
+            
+        Lw: float
+            Soil scalar depolarization factor of water agregates [-]
 
-    This is a effective medium model that uses a differential 
-    approach to compute the electrical conductivity based on the initial 
-    conditions and the water content [1]. 
-
-    Parameters
-    ----------
-    water : array_like
-        Soil volumetric water content [m**3/m**3].
-    ec_init : float
-        Initial soil bulk real electrical conductivity [S/m].
-    wat_init : float
-        Initial soil volumetric water content [m**3/m**3].
-    wc : array_like
-        Soil water real electrical conductivity [S/m].
-    Lw : float
-        Soil scalar depolarization factor of water aggregates (effective medium theory) [-]
-
-    Returns
-    -------
-    array_like
-        The estimated soil bulk real electrical conductivity [S/m].
-
-    Notes
-    -----
-    The Wunderlich model is a differential model and the approach used here 
-    employs a simple while loop with a step of 0.01 until the differential 
-    fraction reaches 1. The method takes into account the initial water content, 
-    initial electrical conductivity, and weighting factors to determine the 
-    bulk electrical conductivity.
-
-    References
-    ----------
-    .. [1] Tina Wunderlich, Hauke Petersen, Said Attia al Hagrey, Wolfgang Rabbel; 
-    Pedophysical Models for Resistivity and Permittivity of Partially Water-Saturated Soils. 
-    Vadose Zone Journal 2013;; 12 (4): vzj2013.01.0023. doi: https://doi.org/10.2136/vzj2013.01.0023
-
-    Example
-    -------
-    >>> WunderlichEC(0.3, 0.08, 0.05, 0.50, 0.01)
-    0.182634
-
-    """
+        Returns
+        -------
+        bulk_ec: float
+            Soil bulk real electrical conductivity [S/m]
+    """     
     diff = water - wat_init                                               # Diference utilized just for simplicity
     bulk_ec = ec_init                                                     # Initial permitivity = Epsilon sub 1  
     x = 0                                                                 # Diferentiation from p = 0  
@@ -62,62 +42,45 @@ def WunderlichEC(water, ec_init, wat_init, wc, Lw):
 
 def Fu(water, clay, bd, pd, wc, solid_ec, dry_ec, sat_ec, s=1, w=2):
     """
-    Calculate the soil bulk real electrical conductivity using the Fu model.
+        Fu et al., 2021 
+        
+        Parameters
+        ----------
+        water: float
+            volumetric water content [-]
 
-    This is a volumetric mixing model that takes into account various soil properties 
-    such as clay content, bulk density, particle density, and water content. 
-    It was exhaustively validated using several soil samples [1].
+        clay: float
+            Soil volumetric clay content [m**3/m**3]
+        
+        bd: float
+            bulk density [g/cm3]
+        
+        pdn: float
+            particle density [g/cm3]
 
-    Parameters
-    ----------
-    water : array_like
-        Soil volumetric water content [m**3/m**3].
-    clay : array_like
-        Soil clay content [g/g]*100.
-    bd : array_like 
-        Soil bulk density (g/cm^3).
-    pd : array_like
-        Soil particle density (g/cm^3).
-    wc : array_like
-        Soil water real electrical conductivity [S/m].
-    solid_ec : array_like
-        Soil solid real electrical conductivity [S/m].
-    dry_ec : array_like
-        Soil bulk real electrical conductivity at zero water content [S/m].
-    sat_ec : array_like
-        Soil bulk real electrical conductivity at saturation water content [S/m].
-    s : float, optional
-        Phase exponent of the solid, default is 1.
-    w : float, optional
-        Phase exponent of the water, default is 2.
+        wc: float
+            Soil water real electrical conductivity [S/m]
+ 
+        solid_ec: float
+            Soil solid real electrical conductivity [S/m]
 
-    Returns
-    -------
-    array_like
-        The estimated bulk electrical conductivity [S/m].
+        dry_ec: float
+            Soil bulk real electrical conductivity with zero water content [S/m]
 
-    Notes
-    -----
-    The method uses default values for s and w, which are 1 and 2 respectively, 
-    but can be modified if necessary. Three different forms of the model are used 
-    depending on the soil data availability. The soil electrical conductivity of solid surfaces 
-    is calculated as in [1] using the formula of Doussan and Ruy (2009) [2]
+        sat_ec: float
+            Soil bulk real electrical conductivity at saturation capacity [S/m]
 
-    References
-    ----------
-    .. [1] Yongwei Fu, Robert Horton, Tusheng Ren, J.L. Heitman,
-    A general form of Archie's model for estimating bulk soil electrical conductivity,
-    Journal of Hydrology, Volume 597, 2021, 126160, ISSN 0022-1694, https://doi.org/10.1016/j.jhydrol.2021.126160.
-    .. [2] Doussan, C., and Ruy, S. (2009), 
-    Prediction of unsaturated soil hydraulic conductivity with electrical conductivity, 
-    Water Resour. Res., 45, W10408, doi:10.1029/2008WR007309.
+        w: float
+            phase exponent of the water [-]
 
-    Example
-    -------
-    >>> Fu(0.3, 30, 1.3, 2.65, 0.3, 0, np.nan, np.nan)
-    0.072626
+        s: float
+            phase exponent of the solid [-]
 
-    """
+        Returns
+        -------
+        bulk_ec: float
+            Soil bulk real electrical conductivity [S/m]
+    """      
     d = 0.6539
     e = 0.0183
     por = 1 - bd/pd
@@ -136,54 +99,29 @@ def Fu(water, clay, bd, pd, wc, solid_ec, dry_ec, sat_ec, s=1, w=2):
     return bulk_ec
 
 
-def LongmireSmithEC(bulk_ec_dc, frequency_ec):
+def LongmireSmithEC(bulk_ec, frequency_ec):
     """
-    Calculate the soil bulk real electrical conductivity using the Longmire-Smith model.
-
-    This is a semiempirical model that calculates the soil bulk real electrical conductivity at different
-    electromagnetic frequencies [1].
-
-    Parameters
-    ----------
-    bulk_ec_dc : array_like
-        Soil bulk real direct current electrical conductivity [S/m].
-    frequency_ec : array_like
-        Frequency of electric conductivity measurement [Hz].
-
-    Returns
-    -------
-    array_like
-        Soil bulk real electrical conductivity [S/m].
-
-    Notes
-    -----
-    The Longmire-Smith equation uses a set of coefficients to account for the 
-    frequency-dependent dielectric dispersion. If all values in the `bulk_ec_dc` 
-    array are zero, the function returns 0.
-
-    Global Variables Used
-    ---------------------
-    epsilon_0 : float
-        The vacuum permittivity constant.
-
-    References
-    ----------
-    .. [1] K. S. Smith and C. L. Longmire, “A universal impedance for soils,” 
-    Defense Nuclear Agency, Alexandria, VA, USA, Topical 
-    Report for Period Jul. 1 1975-Sep. 30 1975, 1975.
-
-    Example
-    -------
-    >>> LongmireSmithEC(np.array([0.05, 0.10]), 130)
-    array([0.05153802, 0.10245936])
-
-    """
-    if (bulk_ec_dc == 0).all():
+        Longmire and Smith, 1975 
+        
+        Parameters
+        ----------
+        bulk_ec: float
+            Soil bulk real direct current electrical conductivity [-]
+            
+        frequency_ec: float
+            frequency of electrical conductivity readings [-]
+   
+        Returns
+        -------
+        bulk_perm: float
+            Soil bulk real relative dielectric permittivity       
+    """               
+    if (bulk_ec == 0).all():
         return 0
     
     else: 
         a = [3.4e6, 2.74e5, 2.58e4, 3.38e3, 5.26e2, 1.33e2, 2.72e1, 1.25e1, 4.8, 2.17, 9.8e-1, 3.92e-1, 1.73e-1]
-        f = (125*bulk_ec_dc)**0.8312
+        f = (125*bulk_ec)**0.8312
         bulk_eci_ = []
         
         for i in range(len(a)):
@@ -191,51 +129,36 @@ def LongmireSmithEC(bulk_ec_dc, frequency_ec):
             bulk_eci = 2*pi*epsilon_0*(a[i]*F_*(frequency_ec/F_)**2/(1+(frequency_ec/F_)**2))                     
             bulk_eci_.append(bulk_eci)
 
-        bulk_ec = bulk_ec_dc + sum(bulk_eci_)
-        return bulk_ec
+        bulk_ec_f = bulk_ec + sum(bulk_eci_)
+        return bulk_ec_f
     
     
-def Rhoades(water, wc, s_ec, E, F):
+def Rhoades(water, water_ec, s_ec, E, F):
     """
-    Calculate the bulk electrical conductivity using the Rhoades model.
+        Rhoades et al., 1976
+        
+        Parameters
+        ----------
+        water: float
+            volumetric water content [-]
 
-    This function estimates the bulk electrical conductivity of a soil-water mixture 
-    using the Rhoades equation [1]. The model combines the contributions of the water's 
-    electrical conductivity and the soil's electrical conductivity, adjusted by 
-    weighting factors based on the water content.
+        water_ec: float
+            Soil water real electrical conductivity [S/m]
+ 
+        s_ec: float
+            Soil bulk real surface electrical conductivity [S/m]
 
-    Parameters
-    ----------
-    water : array_like
-        Soil volumetric water content [m**3/m**3].
-    wc : array_like
-        Soil water real electrical conductivity [S/m].
-    s_ec : array_like
-        Soil bulk real surface electrical conductivity [S/m].
-    E : float
-        Weighting factor for the quadratic term of water content in the Rhoades equation.
-    F : float
-        Weighting factor for the linear term of water content in the Rhoades equation.
+        E: float
+            Empirical constant [-]
 
-    Returns
-    -------
-    array_like
-        Soil bulk real electrical conductivity [S/m].
+        f: float
+            Empirical constant [-]
 
-    References
-    ----------
-    .. [1] Rhoades, J.D., Raats, P.A.C. and Prather, R.J. (1976), 
-    Effects of Liquid-phase Electrical Conductivity, Water Content, 
-    and Surface Conductivity on Bulk Soil Electrical Conductivity. 
-    Soil Science Society of America Journal, 40: 651-655. 
-    https://doi.org/10.2136/sssaj1976.03615995004000050017x
-
-    Example
-    -------
-    >>> Rhoades(0.3, 0.5, 0.001, 1, 0.5)
-    0.121
-
-    """
-    bulk_ec = wc*(E*(water**2)+F*water) + s_ec
+        Returns
+        -------
+        bulk_ec: float
+            Soil bulk real electrical conductivity [S/m]
+    """  
+    bulk_ec = water_ec*(E*(water**2)+F*water) + s_ec
     
     return bulk_ec
--- pedophysics/pedophysical_models/water_ec.py
+++ pedophysics/pedophysical_models/water_ec.py
@@ -1,44 +1,20 @@
 def SenGoode(T, C_f):
     """
-    Calculate soil water real electrical conductivity using the Sen and Goode model.
-
-    Based on the model proposed by Sen and Goode (1992 a, b) [1][2], this function estimates 
-    the oil water real electrical conductivity given the soil temperature and 
-    salinity of the bulk pore fluid.
-
-    Parameters
-    ----------
-    T : array_like
-        Soil bulk temperature [K]
-    C_f : array_like
-        Soil (NaCl) salinity of the bulk pore fluid [mol/L].
-
-    Returns
-    -------
-    water_ec : array_like
-        Soil water real electrical conductivity [S/m]].
-
-    References
-    ----------
-    .. [1] Sen P and Goode P (1992a) 
-    Influence of temperature on electrical conductivity on shaly sands. 
-    Geophysics 57: 89-96.
-    .. [2] Sen P and Goode P (1992b) 
-    Errata, to: Influence of temperature on electrical conductivity of shaly sands. 
-    Geophysics 57: 1658.
-
-    Notes
-    -----
-    The function uses specific coefficients based on the Sen and Goode model to 
-    compute the electrical conductivity of soil water, taking into account the 
-    temperature and salinity effects.
-
-    Example
-    -------
-    >>> SenGoode(298.15, 0.01)
-    0.117822
-    
-    """
+        Sen and Goode (1992 a, b)
+        
+        Parameters
+        ----------
+        temperature: float
+            Soil temperature [K]
+        
+        C_f: float
+            Soil salinity (NaCl) of the bulk pore fluid [mol/L]
+ 
+        Returns
+        -------
+        water_ec: float
+            Soil water real electrical conductivity [S/m]   
+    """   
     T_celsius = T-273.15 
     d1 = 5.6 
     d2 = 0.27 
@@ -49,3 +25,30 @@ def SenGoode(T, C_f):
     water_ec = (d1+d2*T_celsius+d3*T_celsius**2)*C_f - ((d4+d5*T_celsius)/(1+d6*C_f**0.5))*C_f**1.5
 
     return water_ec
+
+
+#def Hilhorst(bulk_ec, bulk_perm, wp, bulk_perm_0):
+    """
+        Hilhorst 2000
+        
+        Parameters
+        ----------
+        bulk_ec: float
+            Soil bulk real electrical conductivity [S/m]
+
+        bulk_perm: float
+            Soil bulk real relative dielectric permittivity   
+        
+        wp: float
+            water permittivity phase [-]
+
+        bulk_perm_0: float
+            Soil bulk real relative dielectric permittivityat zero bulk real electrical conductivity [-]   
+ 
+        Returns
+        -------
+        water_ec: float
+            Soil water real electrical conductivity [S/m]   
+    """   
+#    water_ec = 54444 # not sure of I should write this
+#    return water_ec
